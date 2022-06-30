0f806eee93a7d40f234be613e97e244511d61c63
SidorinAnton1978 <ASidorin1978@gmail.com>
2022-07-01 00:15:43 +0700

    work in process

--- functions.py
+++ functions.py
@@ -1,1 +1,15 @@
 import json
+
+
+def load_json():
+    with open('posts.json', 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def get_by_word(word):
+    result = []
+    for post in load_json():
+        if word.lower() in post['content'].lower():
+            result.append(post)
+    return result
+
--- main/views.py
+++ main/views.py
@@ -1,9 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from functions import get_by_word
 
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
-
 
 
 @main_blueprint.route('/')
 def main_page():
     return render_template('index.html')
+
+
+@main_blueprint.route('/search/')
+def search_page():
+    search_word = request.args.get('s')
+    posts = get_by_word(search_word)
+    return render_template('post_list.html', word=search_word, posts=posts)
+
